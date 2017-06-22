0ace34000bf0c7afd5d50937af34d7a363d0cf3a
jacky <jacky@finereport.com>
2017-06-22 17:14:01 +0800

    single mode

--- rangeseekbarlibrary/src/main/java/com/li/jacky/rangeseekbarlibrary/RangeSeekBar.java
+++ rangeseekbarlibrary/src/main/java/com/li/jacky/rangeseekbarlibrary/RangeSeekBar.java
@@ -10,53 +10,55 @@ import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewConfiguration;
 import java.math.BigDecimal;
 
 /**
  * Created by Jacky on 2017/6/20.
- *  区间滑块
+ * 区间滑块
  */
 
-public class RangeSeekBar<T extends Number> extends AppCompatImageView {
+public class RangeSeekBar<T extends Number> extends View{
 
     private static final Integer DEFAULT_MINIMUM = 0;
     private static final Integer DEFAULT_MAXIMUM = 100;
     private T absoluteMinValue, absoluteMaxValue;
     private double minValuePrim, maxValuePrim;//最值
-    private static final int DEFAULT_TEXT_SIZE_IN_DP = 14;
-    private static final int SEEKBAR_OUTER_LAYER = 24;
-    private static final int SEEKBAR_INNER_LAYER = 8;
-    private  int mPadding = 0;
-    private int mTextSize;
-    private static final int TEXT_OFFSET = 10;
-    private int mScaledTouchSlop;
     private RectF mRect;
-    private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private final Bitmap thumbImage = BitmapFactory.decodeResource(getResources(), R.drawable.seek_thumb_normal);
-    private final float thumbWidth = thumbImage.getWidth();
-    private final float thumbHalfWidth = 0.5f * thumbWidth;
-    private final float thumbHalfHeight = 0.5f * thumbImage.getHeight();
-    private double normalizedMinValue = 0d;
-    private double normalizedMaxValue = 1d;
+    private Paint mPaint;
+    private Bitmap thumbImage;
+    private Bitmap thumbImagePress;
     private OnRangeSeekBarChangeListener<T> listener;
     private int mActivePointerId;
     private float mDownMotionX;
     private Thumb pressedThumb = null;
+    private NumberType mNumberType;
     private boolean mIsDragging;
-    public int ACTION_POINTER_INDEX_MASK = 0x0000ff00, ACTION_POINTER_INDEX_SHIFT = 8;
+    private boolean mIsSingleMode;
+
     private int outRadius;
     private int innerRadius;
     private int distance;
+    private int DEFAULT_TEXT_SIZE_IN_DP = 14;
+    private int SEEK_BAR_OUTER_LAYER = 24;
+    private int SEEK_BAR_INNER_LAYER = 8;
+    private int mPadding = 0;
+    private int mTextSize;
+    private int TEXT_OFFSET = 10;
+    private int mScaledTouchSlop;
+    public int ACTION_POINTER_INDEX_MASK = 0x0000ff00, ACTION_POINTER_INDEX_SHIFT = 8;
     private int outerSeekBarColor = Color.parseColor("#F2F4F5");
     private int innerSeekBarColor = Color.parseColor("#cccccc");
     private int innerActiveSeekBarColor = Color.parseColor("#007aff");
-    private NumberType mNumberType;
+    private float thumbWidth;
+    private float thumbHalfWidth;
+    private float thumbHalfHeight;
+    private double normalizedMinValue = 0d;
+    private double normalizedMaxValue = 1d;
 
     public RangeSeekBar(Context context) {
         super(context);
@@ -81,20 +83,30 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
             setNumberRange(
                 getNumericValueFromAttributes(ta, R.styleable.IFBIRangeSeekBar_MinValue, DEFAULT_MINIMUM),
                 getNumericValueFromAttributes(ta, R.styleable.IFBIRangeSeekBar_MaxValue, DEFAULT_MAXIMUM));
+            mIsSingleMode = ta.getBoolean(R.styleable.IFBIRangeSeekBar_singleThumb, false);
             ta.recycle();
         }
 
-        mTextSize = PixelUtil.dpToPx(context, DEFAULT_TEXT_SIZE_IN_DP);
-        mRect = new RectF();
-        mPadding = (getPaddingLeft() + getPaddingRight()) / 2;
-        outRadius = PixelUtil.dpToPx(getContext(), SEEKBAR_OUTER_LAYER / 2);
-        innerRadius = PixelUtil.dpToPx(getContext(), SEEKBAR_INNER_LAYER / 2);
-        distance = PixelUtil.dpToPx(getContext(), (SEEKBAR_OUTER_LAYER - SEEKBAR_INNER_LAYER) / 2);
+        initRes(context);
 
+    }
+
+    private void initRes(Context context) {
+        mRect = new RectF();
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        thumbImage = BitmapFactory.decodeResource(getResources(), R.drawable.seek_thumb_normal);
+        thumbImagePress = BitmapFactory.decodeResource(getResources(), R.drawable.seek_thumb_press);
+        thumbWidth = thumbImage.getWidth();
+        thumbHalfWidth = 0.5f * thumbWidth;
+        thumbHalfHeight = 0.5f * thumbImage.getHeight();
+        mTextSize = PixelUtil.dpToPx(context, DEFAULT_TEXT_SIZE_IN_DP);
+        mPadding = (getPaddingLeft() + getPaddingRight()) / 2;
+        outRadius = PixelUtil.dpToPx(getContext(), SEEK_BAR_OUTER_LAYER / 2);
+        innerRadius = PixelUtil.dpToPx(getContext(), SEEK_BAR_INNER_LAYER / 2);
+        distance = PixelUtil.dpToPx(getContext(), (SEEK_BAR_OUTER_LAYER - SEEK_BAR_INNER_LAYER) / 2);
         setFocusable(true);
         setFocusableInTouchMode(true);
         mScaledTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
-
     }
 
     private void setNumberRange(T minValue, T maxValue) {
@@ -133,16 +145,38 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = 200;
-        if (MeasureSpec.UNSPECIFIED != MeasureSpec.getMode(widthMeasureSpec)) {
-            width = MeasureSpec.getSize(widthMeasureSpec);
-        }
+        setMeasuredDimension(measure(widthMeasureSpec, true), measure(heightMeasureSpec, false));
+    }
 
-        int height = 300;
-        if (MeasureSpec.UNSPECIFIED != MeasureSpec.getMode(heightMeasureSpec)) {
-            height = Math.max(height, MeasureSpec.getSize(heightMeasureSpec));
+    private int measure(int measureSpec, boolean isWidth) {
+        int result;
+        int mode = MeasureSpec.getMode(measureSpec);
+        int size = MeasureSpec.getSize(measureSpec);
+        int padding = isWidth ? getPaddingLeft() + getPaddingRight() : getPaddingTop() + getPaddingBottom();
+        if (mode == MeasureSpec.EXACTLY) {
+            result = size;
+        } else {
+            result = isWidth ? getSuggestedMinimumWidth() : getSuggestedMinimumHeight();
+            result += padding;
+            if (mode == MeasureSpec.AT_MOST) {
+                if (isWidth) {
+                    result = Math.max(result, size);
+                } else {
+                    result = Math.min(result, size);
+                }
+            }
         }
-        setMeasuredDimension(width, height);
+        return result;
+    }
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        return thumbImage.getHeight() * 2;
+    }
+
+    @Override
+    protected int getSuggestedMinimumHeight() {
+        return thumbImage.getHeight() + PixelUtil.dpToPx(getContext(), TEXT_OFFSET);
     }
 
     @Override
@@ -151,15 +185,19 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
         float minSeekValue = getSeekPosition(normalizedMinValue);
         float maxSeekValue = getSeekPosition(normalizedMaxValue);
 
-        drawSeekBar(canvas, minSeekValue, maxSeekValue );
+        drawSeekBar(canvas, minSeekValue, maxSeekValue);
         //绘制按钮
-        drawThumb(minSeekValue, canvas);
-        drawThumb(maxSeekValue, canvas);
+        if (!mIsSingleMode) {
+            drawThumb(minSeekValue, canvas, Thumb.MIN);
+        }
+        drawThumb(maxSeekValue, canvas, Thumb.MAX);
         //绘制文字
         mPaint.setTextSize(mTextSize);
         mPaint.setColor(Color.parseColor("#1a1a1a"));
-        setThumbValue(normalizedToValue(normalizedMinValue)+"", minSeekValue, canvas);
-        setThumbValue(normalizedToValue(normalizedMaxValue)+"", maxSeekValue, canvas);
+        if (!mIsSingleMode) {
+            setThumbValue(normalizedToValue(normalizedMinValue) + "", minSeekValue, canvas);
+        }
+        setThumbValue(normalizedToValue(normalizedMaxValue) + "", maxSeekValue, canvas);
 
     }
 
@@ -175,12 +213,12 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
         canvas.drawRoundRect(mRect, outRadius, outRadius, mPaint);
 
         mPaint.setColor(innerSeekBarColor);
-        mRect.set(outLeft, outTop + distance, outRight,outBottom - distance);
+        mRect.set(outLeft, outTop + distance, outRight, outBottom - distance);
         canvas.drawRoundRect(mRect, innerRadius, innerRadius, mPaint);
 
         mPaint.setColor(innerActiveSeekBarColor);
         mRect.set(minSeekValue, outTop + distance, maxSeekValue, outBottom - distance);
-        canvas.drawRect(mRect, mPaint);
+        canvas.drawRoundRect(mRect, innerRadius, innerRadius, mPaint);
     }
 
     private void setThumbValue(String text, float seekValue, Canvas canvas) {
@@ -191,17 +229,21 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
 
     /**
      * 绘制滑块
+     *
      * @param seekValue 滑动百分比
      */
-    private void drawThumb(float seekValue, Canvas canvas) {
-        canvas.drawBitmap(thumbImage, seekValue - thumbHalfWidth, getHeight() / 2 - thumbHalfHeight, mPaint);
+    private void drawThumb(float seekValue, Canvas canvas, Thumb thumb) {
+        if (pressedThumb == thumb) {
+            canvas.drawBitmap(thumbImagePress, seekValue - thumbHalfWidth, getHeight() / 2 - thumbHalfHeight, mPaint);
+        } else {
+            canvas.drawBitmap(thumbImage, seekValue - thumbHalfWidth, getHeight() / 2 - thumbHalfHeight, mPaint);
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int pointerIndex;
         int action = event.getAction();
-        Log.i("mtag", "onTouchEvent:   "+action);
         switch (action) {
             case MotionEvent.ACTION_DOWN:
                 mActivePointerId = event.getPointerId(event.getPointerCount() - 1);
@@ -212,6 +254,7 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
                 if (pressedThumb == null) {
                     return super.onTouchEvent(event);
                 }
+
                 setPressed(true);
                 onStartTrackingTouch();
                 trackTouchEvent(event);
@@ -332,7 +375,7 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
 
     /**
      * @param xPosition x坐标
-     * @return  seekbar滑动百分比
+     * @return seekbar滑动百分比
      */
     private double screenToNormalized(float xPosition) {
         int width = getWidth();
@@ -409,7 +452,7 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
         MIN, MAX
     }
 
-    private enum NumberType {
+    private enum NumberType{
         INTEGER, FLOAT, DOUBLE, BIG_DECIMAL;
 
         public static <E extends Number> NumberType fromNumber(E value) throws IllegalArgumentException {
@@ -433,7 +476,7 @@ public class RangeSeekBar<T extends Number> extends AppCompatImageView {
                 case DOUBLE:
                     return value;
                 case FLOAT:
-                    return (float)value;
+                    return (float) value;
                 case INTEGER:
                     return (int) value;
                 case BIG_DECIMAL:
